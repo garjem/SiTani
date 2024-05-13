663537e549c9492a9e4ae8c38a0e815535ee02f8
daffaabdillahsyah <daffaabdillahsyah77@gmail.com>
2024-05-13 22:38:41 +0700

    baru

--- SiTani/routes/web.php
+++ SiTani/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BelanjaController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,6 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('auth.login');
 });
+
+Route::get('/belanja',[BelanjaController::class,'index']);
 
 Auth::routes();
 
