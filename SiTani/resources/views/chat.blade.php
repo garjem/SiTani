60ee1d584b428bdc4a082da11169ebed5539d12c
daffaabdillahsyah <daffaabdillahsyah77@gmail.com>
2024-05-24 22:27:25 +0700

    no message

--- SiTani/resources/views/chat.blade.php
+++ SiTani/resources/views/chat.blade.php
@@ -135,5 +135,11 @@
         padding: 10px;
         cursor: pointer;
     }
+    .contact-item img {
+        width: 40px;
+        height: 40px;
+        border-radius: 50%;
+        margin-right: 10px;
+    }
 
 </style>
