335e6367f0ae108b89f8744c7e7ad82d03c63a37
daffaabdillahsyah <daffaabdillahsyah77@gmail.com>
2024-05-24 12:17:52 +0700

    no message

--- SiTani/resources/views/belanja.blade.php
+++ SiTani/resources/views/belanja.blade.php
@@ -6,7 +6,9 @@
     <title>Document</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <style>
-        
+        .navbar.bg-body-tertiary {
+            background-color: #58A399 !important;
+        }
 
         .card {
             display: flex;
