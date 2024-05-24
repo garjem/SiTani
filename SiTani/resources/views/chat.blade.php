8ba051def89eb8e9344d58d0dda1d81585908030
daffaabdillahsyah <daffaabdillahsyah77@gmail.com>
2024-05-24 22:01:56 +0700

    no message

---
+++ SiTani/resources/views/chat.blade.php
@@ -0,0 +1,6 @@
+<!-- resources/views/chat.blade.php -->
+<div>
+    <!-- Tombol Chat -->
+    <button id="chatButton" class="chat-button">
+        <img src="chat.png" alt="Chat Icon" width="30" height="30">
+    </button>
