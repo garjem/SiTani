94a3db4ebb617a1bf708f35bdf6448be2888e100
stefanario <stefanus877@gmail.com>
2024-05-30 11:57:04 +0700

    Membuat style untuk bagian keranjang

---
+++ SiTani/resources/views/cart.blade.php
@@ -0,0 +1,42 @@
+<style>
+    .card {
+        border: none;
+        box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+    }
+    .table thead th {
+        border-bottom: none;
+    }
+    .table tbody td {
+        vertical-align: middle;
+    }
+    .btn-outline-secondary {
+        background-color: #ffffff;
+        border-color: #0d9488;
+        color: #0d9488;
+    }
+    .btn-outline-secondary:hover {
+        background-color: #0d9488;
+        color: #ffffff;
+    }
+    .btn-danger {
+        background-color: #ef4444;
+        border: none;
+    }
+    .btn-danger:hover {
+        background-color: #dc2626;
+    }
+    .btn-primary {
+        background-color: #0d9488;
+        border: none;
+    }
+    .btn-primary:hover {
+        background-color: #0f766e;
+    }
+    .modal-header, .modal-footer {
+        border: none;
+    }
+    .form-control:focus {
+        border-color: #0d9488;
+        box-shadow: none;
+    }
+</style>
