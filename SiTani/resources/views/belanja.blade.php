872aa4ec921f8b195273e06e429c789f17c59b8f
daffaabdillahsyah <daffaabdillahsyah77@gmail.com>
2024-06-09 22:43:20 +0700

    no message

--- SiTani/resources/views/belanja.blade.php
+++ SiTani/resources/views/belanja.blade.php
@@ -65,42 +65,14 @@
             border-top-right-radius: 25px;
             border-bottom-right-radius: 25px; 
         }
-        
+
         body {
             padding-top: 55px; /* Adjust the padding based on the height of your navbar */
         }
     </style>
 </head>
 <body>
-    <header>
-        <nav class="navbar navbar-expand-lg bg-body-tertiary fixed-top">
-            <div class="container-fluid">
-                <a class="navbar-brand mb-0 h1 text-white" href="#">SiTani</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
-                    <ul class="navbar-nav nav-underline">
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Workshop</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link active text-white" aria-current="page" href="#">Belanja</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Artikel</a>
-                        </li>
-                    </ul>
-                </div>
-                <a class="navbar-brand" href="#">
-                    <img src="keranjang.png" width="30" height="30">
-                </a>
-                <a class="navbar-brand" href="#">
-                    <img src="profile.png" width="30" height="30">
-                </a>
-            </div>
-        </nav>
-    </header>
+    @include('navbar')
     <main>
         <form class="d-flex w-50 mx-auto mb-0 mt-4 search-form" role="search">
             <div class="search-container">
