7c4225023f56ecb9538446eb0f91658167c29a06
daffaabdillahsyah <daffaabdillahsyah77@gmail.com>
2024-06-09 22:43:44 +0700

    no message

---
+++ SiTani/resources/views/navbar.blade.php
@@ -0,0 +1,28 @@
+<!-- resources/views/navbar.blade.php -->
+<nav class="navbar navbar-expand-lg bg-body-tertiary fixed-top">
+    <div class="container-fluid">
+        <a class="navbar-brand mb-0 h1 text-white" href="#">SiTani</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
+            <ul class="navbar-nav nav-underline">
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Workshop</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active text-white" aria-current="page" href="#">Belanja</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Artikel</a>
+                </li>
+            </ul>
+        </div>
+        <a class="navbar-brand" href="#">
+            <img src="keranjang.png" width="30" height="30">
+        </a>
+        <a class="navbar-brand" href="#">
+            <img src="profile.png" width="30" height="30">
+        </a>
+    </div>
+</nav>
