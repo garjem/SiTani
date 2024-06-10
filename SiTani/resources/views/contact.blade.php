73743fe97bcb432ef1d1ffe1d19d7c17c74c4960
daffaabdillahsyah <daffaabdillahsyah77@gmail.com>
2024-06-10 15:09:35 +0700

    no message

--- SiTani/resources/views/contact.blade.php
+++ SiTani/resources/views/contact.blade.php
@@ -18,11 +18,13 @@
             color: white;
             border: none;
             padding: 10px 20px;
-            border-radius: 25px;
             text-align: center;
             font-size: 16px;
             cursor: pointer;
             transition: background-color 0.3s ease;
+            border-radius: 0; /* Ubah border-radius menjadi 0 */
+            width: 100%; /* Set lebar tombol */
+            max-width: 400px; /* Set lebar maksimal tombol */
         }
         .btn-whatsapp:hover {
             background-color: #558b89;
@@ -36,14 +38,18 @@
     @include('/layout/navbar')
     <main>
         <div class="container mt-5 text-center">
-            <h1>Pengajuan lebih lanjut</h1>
-            <a href="https://wa.me/6281287157805" class="btn-whatsapp mt-3">Whatsapp</a>
-
-            <h1 class="mt-5">Informasi lebih lanjut</h1>
-            <a href="https://wa.me/6281287157805" class="btn-whatsapp mt-3">Whatsapp</a>
-
-            <h1 class="mt-5">Konsultasi lebih lanjut</h1>
-            <a href="https://wa.me/6281287157805" class="btn-whatsapp mt-3">Whatsapp</a>
+            <h1><strong>Pengajuan lebih lanjut</strong></h1>
+            <div class="d-flex justify-content-center">
+                <a href="https://wa.me/6281287157805" class="btn-whatsapp mt-3">Whatsapp</a>
+            </div>
+            <h1 class="mt-5"><strong>Informasi lebih lanjut</strong></h1>
+            <div class="d-flex justify-content-center">
+                <a href="https://wa.me/6281287157805" class="btn-whatsapp mt-3">Whatsapp</a>
+            </div>
+            <h1 class="mt-5"><strong>Konsultasi lebih lanjut</strong></h1>
+            <div class="d-flex justify-content-center">
+                <a href="https://wa.me/6281287157805" class="btn-whatsapp mt-3">Whatsapp</a>
+            </div>
         </div>
         <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js"></script>
