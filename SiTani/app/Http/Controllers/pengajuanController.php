8631ea6fc47f7f41f900494381344b165efb7bdd
daffaabdillahsyah <daffaabdillahsyah77@gmail.com>
2024-06-10 14:33:04 +0700

    no message

---
+++ SiTani/app/Http/Controllers/pengajuanController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class pengajuanController extends Controller
+{
+    public function pengajuan(){
+        return view('pengajuan');
+    }
+}
