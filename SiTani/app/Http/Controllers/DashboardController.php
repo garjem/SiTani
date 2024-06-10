9f655e6c61ec878f13286b9420e5c33722f440d6
azrwinata <azrwinata18@gmail.com>
2024-06-10 22:08:36 +0700

    no message

--- SiTani/app/Http/Controllers/DashboardController.php
+++ SiTani/app/Http/Controllers/DashboardController.php
@@ -12,20 +12,20 @@ class DashboardController extends Controller
         $users = User::all();
         return view('\admin\dashboard',compact('users'));
     }
-    public function index1(){
-        $articles = User::all();
-        return view('\admin\dashboardartikel',compact('articles'));
-    }
-    public function index2(){
-        $users = User::all();
-        return view('\admin\dashboarmitra',compact('mitras'));
-    }
+    // public function index1(){
+    //     $articles = User::all();
+    //     return view('\admin\dashboardartikel',compact('articles'));
+    // }
+    // public function index2(){
+    //     $users = User::all();
+    //     return view('\admin\dashboarmitra',compact('mitras'));
+    // }
     public function index3(){
         $products = Product::all();
         return view('\admin\dashboardproduk',compact('products'));
     }
-    public function index4(){
-        $users = User::all();
-        return view('\admin\dashboarworkshop',compact('workshops'));
-    }
+    // public function index4(){
+    //     $users = User::all();
+    //     return view('\admin\dashboarworkshop',compact('workshops'));
+    // }
 }
