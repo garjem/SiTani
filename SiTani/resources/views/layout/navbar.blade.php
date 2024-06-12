fd04906d9dccff746e9739aa51381a2adaed0a47
daffaabdillahsyah <daffaabdillahsyah77@gmail.com>
2024-06-12 18:05:43 +0700

    no message

--- SiTani/resources/views/layout/navbar.blade.php
+++ SiTani/resources/views/layout/navbar.blade.php
@@ -68,7 +68,7 @@
   </div>
 </div>
 
-modal 2
+<!-- modal 2 -->
 <div class="modal fade" id="exampleModal2" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog modal-dialog-centered">
     <div class="modal-content">
